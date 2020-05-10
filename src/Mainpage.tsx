23f49d15900674f28e45b04f9887589f9e6f5bca
Thomas Legrand <legrand.thomas5@hotmail.fr>
2020-05-10 23:34:18 +0200

    add warning message

--- src/Mainpage.tsx
+++ src/Mainpage.tsx
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Card, Layout, List, Menu, Modal } from 'antd';
+import { Card, Layout, List, Menu, Modal, message } from 'antd';
 import * as d3 from 'd3';
 import React, { useEffect, useRef, useState } from 'react';
 import { agents } from './Agents';
@@ -71,15 +71,18 @@ export const MainPage = () => {
 
     resetMap(svg, selectedAgent);
 
-    trajectories
+    const agentTrajectory = trajectories
       .get(selectedMap)!
       .find(
-        (PlayerTrajectory: AgentTrajectory) =>
-          PlayerTrajectory.agent === selectedAgent
-      )
-      ?.trajectories.forEach((trajectory: Trajectory) =>
-        addTrajectory(svg, trajectory, selectedAgent, onTrajectoryClicked)
+        (AgentTrajectory: AgentTrajectory) =>
+          AgentTrajectory.agent === selectedAgent
       );
+
+    agentTrajectory
+      ? agentTrajectory.trajectories.forEach((trajectory: Trajectory) =>
+          addTrajectory(svg, trajectory, selectedAgent, onTrajectoryClicked)
+        )
+      : message.warn(`no stuff for ${selectedAgent} on ${selectedMap} yet.`);
   }, [selectedAgent]);
 
   function clicked(this: any) {
@@ -143,7 +146,11 @@ export const MainPage = () => {
           </StyledMap>
         )}
         <Footer style={{ textAlign: 'center' }}>
-          <div>Created by Thomas Legrand ©{new Date().getFullYear()}</div>
+          <div>
+            Created by{' '}
+            <a href='https://www.thomaslegrand.tech'>Thomas Legrand</a> ©
+            {new Date().getFullYear()}
+          </div>
           <div>
             Credits: <a href='https://www.proguides.com'>ProGuides Valorant</a>,
             <a href='https://www.youtube.com/channel/UCWpzQrZm1zKTX8w7fkgZwLQ'>
